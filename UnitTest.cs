a186ca84d9d8e6f66a3203535b24d59e2d8a179e
manasa.somayya <manasa.somayya@indiumsoft.com>
2022-08-09 20:04:55 +0530

    updated code in testmethods

--- UnitTest.cs
+++ UnitTest.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 
 namespace JenkinsTraining
@@ -6,22 +8,29 @@ namespace JenkinsTraining
     [TestClass]
     public class UnitTest
     {
-        [TestMethod, TestCategory("First")]
-        public void TestMethod1()
+        public IWebDriver driver;
+
+        [TestMethod, TestCategory("First"), Priority(1)]
+        public void TestMethod1() 
         {
-            Console.WriteLine("First Test Method");
+            ChromeOptions options = new ChromeOptions();
+            options.BinaryLocation = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
+            options.AddUserProfilePreference("disable-popup-blocking", "true");
+            driver = new ChromeDriver(options);
         }
 
 
-        [TestMethod, TestCategory("Second")]
+        [TestMethod, TestCategory("Second"), Priority(2)]
         public void TestMethod2()
         {
+            driver.Navigate().GoToUrl("http://www.google.com");
             Console.WriteLine("Second Test Method");
         }
 
         [TestMethod, TestCategory("Third")]
         public void TestMethod3()
         {
+            driver.SwitchTo().ActiveElement().SendKeys("Google");
             Console.WriteLine("Third Test Method");
         }
     }
